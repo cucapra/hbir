03d64c45bc3dd2e18206a32272c0df8cd5bd885c
Edwin Peguero <emp238@cornell.edu>
2019-06-12 17:16:43 -0400

    began refactoring C code string operations into module 'src/core_emit.ml', which is called by f1 host code compiler (now, 'f1_host.ml'). Cleaned up some tab and new lines, and fixed a bug where output arrays were not received properly.

---
+++ lotus/src/core_emit.ml
@@ -0,0 +1,124 @@
+(* Output String Templates *)
+let (#%) = Printf.sprintf
+
+let rec expr_emit (e : Ast.expr) : string =
+  let binop_emit (binop : Ast.binop) : string =
+    match binop with
+        | Ast.Plus -> "+"
+        | Ast.Minus -> "-"
+        | Ast.Mul -> "*"
+        | Ast.Div -> "/"
+        | Ast.Eq -> "=="
+        | Ast.Neq -> "!="
+        | Ast.Lt -> "<"
+        | Ast.Gt -> ">"
+        | Ast.Lteq -> "<="
+        | Ast.Gteq -> ">="
+        | Ast.And -> "&&"
+        | Ast.Or -> "||" in
+
+  begin match e with
+  | VarExpr str -> str
+  | IntExpr v -> string_of_int v
+  | FloatExpr v -> string_of_float v
+  | BoolExpr b -> if b then "true" else "false"
+  | DerefExpr (e, es) -> deref_emit e es
+  | BinAppExpr (binop, e1, e2) -> 
+      "%s %s %s" #%
+         (expr_emit e1) (binop_emit binop) (expr_emit e2)
+  end
+
+and deref_emit (array_expr : Ast.expr) (dims : Ast.expr list) : string =
+  let dims_emitted : string = 
+    List.map expr_emit dims |>
+    List.map ((#%) "[%s]") |>
+    String.concat "" in 
+  expr_emit array_expr ^ dims_emitted
+
+let type_emit (g : Ast.typ) : string =
+    match g with
+    | BoolTyp -> "boolean"
+    | IntTyp -> "int"
+    | FloatTyp -> "float"
+
+let inout_dir_emit (dir : Ast.inout_dir) : string = 
+  match dir with
+  | In -> "deviceToHost"
+  | Out -> "hostToDevice"
+
+
+let array_access_brackets_emit (dims : string list) : string = 
+  List.map ((#%) "[%s]") dims |> String.concat ""
+
+let array_decl_emit (tau : string) (x : string) (dims : string list) =
+  "%s %s%s" #% tau x (array_access_brackets_emit dims)
+
+
+let header_emit (header_file : string) : string = 
+  "#include %s" #% header_file
+
+let expr_macro_emit
+  (var_name : string) (e : Ast.expr) : string = 
+    "#define %s %s" #% var_name (expr_emit e)
+
+
+let var_init_emit (typ : Ast.typ) (x : string) (e : Ast.expr) : string = 
+  "%s %s = %s;\n" #% (type_emit typ) x (expr_emit e)
+
+let dynamic_alloc_array_emit (typ : Ast.typ)
+                       (x : string)
+                       (dims: Ast.expr list) : string =
+  let typ_name : string = type_emit typ in
+  let size_e_emitted : string = 
+    List.map expr_emit dims
+    |> String.concat " * " in
+  "%s *%s = (%s*)malloc(%s * sizeof(%s));" #%
+    typ_name x typ_name size_e_emitted typ_name
+
+
+let return_emit (e : Ast.expr) : string = 
+  "return %s;" #% (expr_emit e)
+
+
+(* Host Emit Chunks *)
+let host_main_decl_emit : string =
+"int main(int argc, char *argv[]) {
+\tassert(argc == 2);
+\tchar *manycore_program = argv[1];
+
+\t// Initialize and get userspace pointer to FPGA
+\tuint8_t fd;
+\thammaInit(&fd);"
+
+let host_blocked_layout_init_emit : string = 
+"// 2D Blocked Layout Init
+int num_cores = (X2 - X1) * (Y2 - Y1);
+int dim_per_core = dim / num_cores;\n"
+
+let host_blocked_layout_send_emit (x: string)
+                                 (typ: Ast.typ) 
+                                 (dir : Ast.inout_dir) : string =
+  let dir_str = 
+    match dir with
+    | In -> "Send"
+    | Out -> "Receive" in
+"// 2D Blocked Layout %s of Array %s
+for (int y = Y1; y < Y2; y++) {
+  for (int x = X1; x < X2; x++) {
+    // This offset sends blocks vectors across cores
+    // in column-major order
+    int offset = (y-Y1) * (X2-X1) * dim_per_core +
+                 (x-X1) * dim_per_core;
+
+    hammaSymbolMemcpy(fd, x, y, manycore_program, \"%s\",
+                      (void*)(%s + offset),
+                      dim_per_core * sizeof(%s),
+                      %s);
+  }
+}\n" #% dir_str x x x 
+        (type_emit typ) (inout_dir_emit dir)
+
+let host_execute_code_blocks_emit : string =
+"// Run all of the tiles
+hammaRunMultiple(fd, X1, Y1, X2, Y2);"
+
---
+++ lotus/src/f1_host.ml
@@ -0,0 +1,151 @@
+let indent (n : int) (s : string) : string =
+  let rec indentation_builder (n : int) : string = 
+    if n == 0 then ""
+    else "\t" ^ indentation_builder (n-1) in
+  let indentation : string = indentation_builder n in
+  let lines : string list = String.split_on_char '\n' s in
+  let indented_lines = List.map ((^) (indentation)) lines in
+  String.concat "\n" indented_lines
+
+let emit (prog: Ast.program) : string = 
+  (* Errors *)
+  let unsupported_many_tile_groups_error : string =
+    "Target Section Error: " ^
+    "only one tile group is supported atm." in
+  let only_two_dims_error : string =
+    "Target Section Error: " ^ 
+    "only 2D tile groups are supported atm." in
+  let unsupported_layout_error : string =
+    "Data Section Error: only Blocked layout supported." in
+
+  (* 1. Headers *)
+  let host_header_emit : string = 
+    let header_files = ["\"f1_helper\""; "<stdlib.h>"] in
+    String.concat "\n" (List.map Core_emit.header_emit header_files) in
+
+  (* 2. Constant Macros *)
+  let host_tile_size_constants_emit (ts : Ast.target_section) : string = 
+    match ts with
+    | [] -> ""
+    | targ::targs ->
+        if List.length targs >= 1
+          then failwith unsupported_many_tile_groups_error
+          else
+            match targ with
+            | GlobalMemDecl _ -> ""
+            | TileDecl tile ->
+              if List.length tile.tile_dims != 2
+                then failwith only_two_dims_error
+                else
+                  let tile_size_consts : (string * Ast.expr) list= 
+                    [("X1", IntExpr 0);
+                     ("X2", List.nth tile.tile_dims 0);
+                     ("Y1", IntExpr 0);
+                     ("Y2", List.nth tile.tile_dims 1)] in
+                    List.map
+                      (fun decl -> 
+                        let (x, e) = decl in 
+                        Core_emit.expr_macro_emit x e) tile_size_consts
+                    |> String.concat "\n" in
+
+
+  (* 4. Array Decls *)
+  let host_data_decls_emit (ds : Ast.data_section) : string =
+    let constant_decls_emit : string = 
+      let constant_decl_emit 
+        (const_decl : (Ast.typ * string * Ast.expr)) =
+        match const_decl with
+        (typ, x, e) -> Core_emit.var_init_emit typ x e in
+      String.concat "" (List.map constant_decl_emit ds.ds_constant_decls) in
+
+    let data_decls_emit : string = 
+      let data_decl_emit (d : Ast.data_decl) : string =
+        Core_emit.dynamic_alloc_array_emit 
+          d.data_type d.data_name d.data_dims in
+      String.concat "\n" (List.map data_decl_emit ds.ds_data_decls) in
+
+    constant_decls_emit ^ 
+    "// Initialize Arrays" ^
+    "\n" ^
+    data_decls_emit in
+
+  (* 5. Input Array Layouts *)
+  let host_input_data_layouts_emit (ds : Ast.data_section) : string =
+    let layout_init_emitted : string =
+      if List.exists 
+          (fun d -> d.Ast.data_layout == Ast.Blocked) ds.ds_data_decls 
+        then Core_emit.host_blocked_layout_init_emit
+      else if List.length ds.ds_data_decls == 0 
+        then "" 
+      else failwith unsupported_layout_error in
+
+    let input_layouts_emitted : string =
+      let layout_emit (d : Ast.data_decl) : string =
+        match d.data_layout with
+        | Blocked -> 
+            Core_emit.host_blocked_layout_send_emit 
+              d.data_name d.data_type d.data_dir
+        | _ -> failwith unsupported_layout_error in
+      let input_data_decls =
+        List.filter 
+          (fun d -> d.Ast.data_dir == Ast.In) ds.ds_data_decls in
+      String.concat "\n" (List.map layout_emit input_data_decls) in
+
+    layout_init_emitted ^
+    "\n" ^
+    input_layouts_emitted in
+
+
+  (* 6. Start execution of code blocks and wait *)
+  let host_execute_blocks_emit = Core_emit.host_execute_code_blocks_emit in
+
+  (* 7. Output Array Decl and Layout *)
+  let host_output_data_layouts_emit (ds : Ast.data_section) : string =
+    let layout_emit (d : Ast.data_decl) : string =
+      match d.data_layout with
+      | Blocked -> 
+          Core_emit.host_blocked_layout_send_emit 
+            d.Ast.data_name 
+            d.data_type 
+            d.data_dir
+      | _ -> failwith unsupported_layout_error in
+    let output_data_decls =
+      List.filter (fun d -> d.Ast.data_dir == Ast.Out) ds.ds_data_decls in
+    String.concat "\n" (List.map layout_emit output_data_decls) in
+
+  (* 8. Do stuff with output *)
+  let host_return_emit = Core_emit.return_emit (Ast.IntExpr 0) in
+
+  (* 1. Headers *)
+  indent 0 host_header_emit ^ 
+  "\n\n" ^
+
+  (* 2. Constant Macros 
+   * Currently, target section macros. 
+   * Do these need to be macros? Should other variables also be macros?
+   * Or should all macros be variables? *)
+  indent 0 (host_tile_size_constants_emit prog.target_section) ^ 
+  "\n\n" ^
+
+  (* 3. Main declaration and FPGA init: *)
+  indent 0 Core_emit.host_main_decl_emit ^ 
+  "\n\n" ^
+
+  (* 4. Array Decls *)
+  indent 1 (host_data_decls_emit prog.data_section) ^ 
+  "\n\n" ^
+
+  (* 5. Input Array Layouts *)
+  indent 1 (host_input_data_layouts_emit prog.data_section) ^ 
+  "\n\n" ^
+
+  (* 6. Start execution of code blocks and wait *)
+  indent 1 host_execute_blocks_emit ^ 
+  "\n\n" ^
+
+  (* 7. Output Array Decl and Layout *)
+  indent 1 (host_output_data_layouts_emit prog.data_section) ^ 
+  "\n\n" ^
+
+  (* 8. Do stuff with output? Or Nothing? *)
+  indent 1 host_return_emit ^ "\n}"
