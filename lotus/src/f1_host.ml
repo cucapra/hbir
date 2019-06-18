3f27998fe7c5f88375df526c39243cfff7955e98
Edwin Peguero <emp238@cornell.edu>
2019-06-18 11:31:23 -0400

    cleaned up 'src/f1_host.ml'; it now emits well formatted code. Correctness will be checked after device code emitter ('src/f1_device.ml') is implemented.

--- lotus/src/f1_host.ml
+++ lotus/src/f1_host.ml
@@ -43,31 +43,37 @@ let emit (prog: Ast.program) : string =
     |> String.concat "\n\n" in
 
 
-  let do_fun_def_emit (fun_name : string) (ds : Ast.data_decl list) : string =
     let (#%) = Core_emit.(#%) in
 
     let error_cascade (ds : string list) : string =
       match ds with 
       | [] -> ""
       | x::xs -> 
-          ("err = %s;" #% x) ::
+          ("err  = %s;" #% x) ::
           (List.map (fun s -> "err |= %s;" #% s) xs)
           |> String.concat "\n" in
 
     let error_check_emit (err : string) : string =
-      Core_emit.unindent
-        "if (err) {
-            \tfprintf(stderr, \"%s\n\");
-            \treturn err;
-        }" #% err in 
+      let if_emit cond body = 
+        "if(%s){\n%s\n}" #% cond (Core_emit.indent 1 body) in
+      if_emit "err"
+        ("fprintf(stderr, \"%s\"); 
+          return err;" #% err
+         |> Core_emit.unindent) in
 
+  let do_fun_def_emit (fun_name : string) (ds : Ast.data_decl list) : string =
     let dims_product_emit (dims : Ast.expr list) : string =
       dims
       |> List.map Core_emit.expr_emit
       |> String.concat " * " in
 
     let eva_init_emit : string =
-      let eva_decl : string =
+      let eva_init_comment : string = Core_emit.unindent
+        "// Allocate space on the device for the three arguments we'll pass to the
+         // function. \"EVA\" is for \"endpoint virtual address,\" and it represents
+         // an address in the device's memory." in
+
+      let eva_decls : string =
         "eva_t %s;" #% (ds
           |> List.map (fun d -> (d.Ast.data_name ^ "_addr"))
           |> String.concat ", ") in
@@ -78,32 +84,44 @@ let emit (prog: Ast.program) : string =
         |> List.map (fun d ->
              "hb_mc_device_malloc(%%device, %s * sizeof(int32_t), %s)" #% 
               (dims_product_emit d.Ast.data_dims) d.Ast.data_name)  
-        |> error_cascade
-        |> (^) ("\n" ^ error_check_emit "hb_mc_device_malloc failed") in
+        |> error_cascade in
 
-      [eva_decl; 
-       eva_dyn_init]
+      [eva_init_comment;
+       eva_decls; 
+       eva_dyn_init;
+       error_check_emit "hb_mc_device_malloc failed"]
       |> String.concat "\n" in
-
-    let input_arrays : Ast.data_decl list = 
-      ds
-      |> List.filter (fun d -> d.Ast.data_dir == Ast.In) in
 
     let hb_mc_device_memcpy_emit (d : Ast.data_decl) : string =
       let name = d.Ast.data_name in
       let size_expr_emit = dims_product_emit d.Ast.data_dims in
-      "hb_mc_device_memcpy(&device, (void*)((intptr_t)%s_addr),\
+      "hb_mc_device_memcpy(&device, (void*)((intptr_t)%s_addr),
           \t\t&%s, %s * sizeof(int32_t), %s)" #% 
           name name size_expr_emit (Core_emit.inout_dir_emit d.Ast.data_dir) in
 
     let input_send_emit : string =
+      let input_send_comment : string = Core_emit.unindent
+        "// Copy input data into the newly allocated space.
+        // TK Why does the `eva_t` need to be converted to a `void*` here?
+        // Shouldn't this function take an `eva_t` as an argument?" in
+
       let input_send_memcpy_emit : string =
-        input_arrays
+        ds
+        |> List.filter (fun d -> d.Ast.data_dir == Ast.In)
         |> List.map hb_mc_device_memcpy_emit
-        |> error_cascade
-        |> (^) ("\n" ^ error_check_emit "hb_mc_device_memcpy failed") in
+        |> error_cascade in
 
       let grid_init_emit : string =
+        let grid_init_args_comment : string = Core_emit.unindent
+          "// The arguments to pass to the device-side function. While the type here
+          // is uint32_t, these are all actually pointers---arguments must be
+          // word-sized numbers." in
+
+        let grid_init_comment : string = Core_emit.unindent
+          "// Set up the tile group, dimensions, and function to call. The last two
+          // arguments to `hb_mc_grid_init` specify the arguments to the `add`
+          // function in the device code." in
+
         let grid_init_args_emit : string =
           "uint32_t args[] = {%s};" #%
             (ds
@@ -111,14 +129,18 @@ let emit (prog: Ast.program) : string =
              |> String.concat ", ") in
 
         (* TODO: what does 'grid_dim' mean?*)
-        grid_init_args_emit ^
+        grid_init_args_comment ^ "\n" ^
+        grid_init_args_emit ^ "\n\n" ^
+        grid_init_comment ^ "\n" ^
         "hb_mc_dimension_t grid_dim = {.x = 1, .y = 1};
         hb_mc_dimension_t tg_dim = {.x = MESH_X, .y = MESH_Y};
         err = hb_mc_grid_init(&device, grid_dim, tg_dim, \"%s\", %s, args);
         if (err) return err;" #% fun_name (List.length ds |> string_of_int) 
         |> Core_emit.unindent in
 
-      [input_send_memcpy_emit;
+      [input_send_comment;
+       input_send_memcpy_emit;
+       error_check_emit "hb_mc_memcpy to device failed" ^ "\n";
        grid_init_emit]
       |> String.concat "\n" in
 
@@ -127,22 +149,26 @@ let emit (prog: Ast.program) : string =
       err = hb_mc_device_tile_groups_execute(&device);
       if (err) return err;" |> Core_emit.unindent in
 
-    let output_arrays : Ast.data_decl list = 
-      ds
-      |> List.filter (fun d -> d.Ast.data_dir == Ast.In) in
-
     let output_receive_emit : string =
-      let output_receive_memcpy_emit : string =
-        output_arrays
-        |> List.map hb_mc_device_memcpy_emit
-        |> error_cascade
-        |> (^) ("\n" ^ error_check_emit "hb_mc_device_memcpy failed") in
+      let output_receive_comment : string = Core_emit.unindent
+        "// Collect the result by copying \
+        output data back over from the device." in
 
-      [output_receive_memcpy_emit;
+      let output_receive_memcpy_emit : string =
+        ds
+        |> List.filter (fun d -> d.Ast.data_dir == Ast.Out)
+        |> List.map hb_mc_device_memcpy_emit
+        |> error_cascade in
+
+      [output_receive_comment;
+       output_receive_memcpy_emit;
        error_check_emit "hb_mc_device_memcpy to host failed"]
       |> String.concat "\n" in
 
-    let cleanup_emit = Core_emit.return_emit (Ast.IntExpr 0) in
+    let cleanup_emit : string = 
+      let cleanup_comment = "// Clean up." in
+      cleanup_comment ^ "\n" ^
+      Core_emit.return_emit (Ast.IntExpr 0) in
   
     (* TODO: 32_t in type? *)
     let params : (string * string) list =
