2394143fc2a35d26cd0a564da70d46daa446157f
Edwin Peguero <emp238@cornell.edu>
2019-08-21 14:01:56 -0400

    initial commit to new branch: we will implement code section code gen here

--- lotus/src/group.ml
+++ lotus/src/group.ml
@@ -3,8 +3,9 @@ type abs_arrangement = {
   abs_arr_groups : group_array list }
 
 and group_array = {
+  ga_rel_name : string;
   ga_dim_bounds : int list;
-  ga_indexed_groups : (ga_index * abs_group) list }
+  ga_indexed_groups : indexed_group list }
 
 and abs_group = {
   g_rel_name : string; (* relative name; absolute name computable with index *)
@@ -14,6 +15,7 @@ and abs_group = {
   g_subgroups : group_array list }
 
 and ga_index = int list 
+and indexed_group = ga_index * abs_group
 
 let rec ga_index_to_string ix : string = 
   begin match ix with
@@ -55,7 +57,8 @@ let arrange_decl_to_abs_arrangement (arr_decl : Ast.arrange_decl)
         List.map (fun ix -> List.combine ix_names ix) ixs, ix_bounds in
 
     (* one relative group corresponds to many absolute groups (i.e., group array) *)
-    { ga_dim_bounds = dim_bounds;
+    { ga_rel_name = g_decl.Ast.gd_name;
+      ga_dim_bounds = dim_bounds;
       ga_indexed_groups = 
         List.map 
         (rel_ix_group_to_abs_ix_group par_ix_ctxt par_size_ctxt g_decl par_origin) 
@@ -67,7 +70,7 @@ let arrange_decl_to_abs_arrangement (arr_decl : Ast.arrange_decl)
                                   (par_size_ctxt : Utils.ctxt)
                                   (g_decl : Ast.group_decl)
                                   (abs_offset : int * int)
-                                  (ix_ctxt : Utils.ctxt) : ga_index * abs_group =
+                                  (ix_ctxt : Utils.ctxt) : indexed_group =
     let abs_row_offset, abs_col_offset = abs_offset in 
     let row_size_name, row_range = g_decl.Ast.gd_row_range in
     let col_size_name, col_range = g_decl.Ast.gd_col_range in
@@ -129,11 +132,41 @@ let abs_arrangement_from_prog (prog : Ast.program) : abs_arrangement =
     (List.hd prog.config_section) 
     (List.nth tile_grid_size 0, List.nth tile_grid_size 1)
 
+let match_group_array_pattern (arr : abs_arrangement) 
+                              (pattern : group_pattern) : group_array list =
+
+  let match_group_array_name (name : string)
+                             (ga_list : group_array list) : group_array list =
+    List.filter (fun ga -> ga.ga_rel_name == name) ga_list in
+
+  let match_indexed_group (ix_pattern : ix_pattern)
+                          (ix : indexed_group) : bool =
+    let match_group_ix_element_pattern (ix_el_pat : ix_element_pattern)
+                                       (ix : int) : bool =
+      match ix_el_pat with
+      | SymIx x -> true
+      | ConcreteIx n -> ix == n in
+    let ix, 
+
+  ga.ga_indexed_groups
+
+  match pattern with
+  | GroupNamePattern rel_group_name index_pattern the_rest -> 
+
+
+    let index_filter: (ga : group_array list) 
+                      (ix_element_pat : ga_index_element_pattern) 
+                      (dim : int) : group_array list =
+      match sym_index with
+      | ConcreteIx n::ixs -> 
+      | SymbolicIx x::ixs ->
+
+
 
 (* dumps groups on command line *)
 let print_abs_arrangement (a : abs_arrangement) : unit =
   let rec print_abs_group_array (ga : group_array) : string =
-    let print_ix_group (ix_group : ga_index * abs_group) : string =
+    let print_ix_group (ix_group : indexed_group) : string =
       let index, g = ix_group in
       let row_min, row_max = g.g_abs_row_range in
       let col_min, col_max = g.g_abs_col_range in
